12749db44bb0c883868211c2d53aec2ca475546f
Camilo Velasquez <camilo.velasquezp@gmail.com>
2019-03-23 12:13:58 -0500

    Se ajusta borrado de relaciones con valor null

--- api/models.py
+++ api/models.py
@@ -18,7 +18,7 @@ class TipoRecurso(models.Model):
 class Encargado(models.Model):
     nombre = models.CharField(max_length=60)
     telefono = models.BigIntegerField()
-    tipo = models.ForeignKey(TipoEncargado, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoEncargado, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.nombre
@@ -33,13 +33,13 @@ class Proveedor(models.Model):
 
 class Recurso(models.Model):
     serial = models.CharField(max_length=100)
-    tipo = models.ForeignKey(TipoRecurso, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoRecurso, null=True, blank=True, on_delete=models.SET_NULL)
     marca = models.CharField(max_length=100)
-    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor, null=True, blank=True, on_delete=models.SET_NULL)
     fechaCompra = models.DateField(auto_now=True)
     estado = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=200)
-    encargado = models.ForeignKey(Encargado, on_delete=models.CASCADE)
+    encargado = models.ForeignKey(Encargado, null=True, blank=True, on_delete=models.SET_NULL)
     fechaAsignacion = models.DateField(auto_now=True)
 
     def __str__(self):
